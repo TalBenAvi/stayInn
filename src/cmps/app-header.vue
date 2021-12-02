c2071b7fdd2bcd5408c50d6cfdad66c8b61ae646
Einav Cohen <einavsprojects@gmail.com>
2021-12-02 09:06:14 +0200

    top fold and initial nav nar design

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,21 +1,73 @@
 <template>
   <header class="app-header">
-    <nav>
-    </nav>
-    <!-- <section class="loggedin-user" v-if="loggedInUser">
-      <router-link :to="`/user/${loggedInUser._id}`">
-        {{ loggedInUser.fullname }}
-      </router-link>
-      <span>{{ loggedInUser.score }}</span>
-    </section> -->
+    <div class="logo">
+      
+      <img :class="logo" :src="require(`@/assets/imgs/branding/${imgSrc}.png`)" />
+    </div>
+
+    <div class="initial-search-bar">
+      <p>Start your search</p>
+      <div class="circle">
+        <img class="search-icon" src="../assets/imgs/icons/search-circle.png" />
+      </div>
+    </div>
+
+    <div class="host-options">
+      <p class="become">Become a Host</p>
+    </div>
+
+    <div class="global">
+      <img src="../assets/imgs/icons/global.png" />
+    </div>
+
+    <div class="user-options">
+      <div class="burger">☰</div>
+      <img class="avatar" src="../assets/imgs/icons/avatar.png" />
+    </div>
+    
   </header>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      windowWidth: window.innerWidth,
+      imgSrc: 'airbnb-logo',
+    }
+
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.onResize)
+
+    })
+
+  },
+  methods: {
+    onResize() {
+      this.windowWidth = window.innerWidth
+      if(this.windowWidth <=980) {
+          this.imgSrc = 'airbnb'
+      } else {
+        this.imgSrc ='airbnb-logo'
+      }
+    }
+  },
   computed: {
-    loggedInUser() {
-      return this.$store.getters.loggedinUser
-    },
+    logo() {
+      if (this.imgSrc === 'airbnb') {
+        return 'logo-icon'
+      } else {
+        return 'logo-img'
+      }
+    }
+  },
+
+  watch: {
+    windowWidth(newWidth, oldWidth) {
+      console.log(`changed from ${oldWidth} to ${newWidth} `)
+    }
   }
 };
 </script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="home">
-  </div>
+  <section class="home home-layout">
+    <div class="top-fold">
+      <div class="home-img">
+        <img src="~@/assets/imgs/home-page/main.jpg">
+              <div class="slogan">
+        <p>Not sure where to go? Perfect.</p>
+      </div>
+      </div>
+
+      <button class="flexible-btn"><span>i'm flexible</span></button>
+      
+    </div>
+  </section>
 </template>
 
 <script>
