89e146233005af25c31a7f15f01e7d42c4cf811a
Yaniv Richman <yaniv4980@gmail.com>
2021-12-02 18:36:21 +0200

    gfd

--- src/app.vue
+++ src/app.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <app-header />
     <router-view/>
+    <app-footer/>
   </div>
 </template>
 
 <script>
 import appHeader from './cmps/app-header.vue'
+import appFooter from './cmps/app-footer.vue'
 export default{
   created() {
 
   },
   components: {
     appHeader,
+    appFooter,
   }
 }
 </script>
