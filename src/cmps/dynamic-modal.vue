b17c0dc0c91e1395a9feeb20a848f18545f797ea
Einav Cohen <einavsprojects@gmail.com>
2021-12-04 18:44:52 +0200

    added modals

--- src/cmps/dynamic-modal.vue
+++ src/cmps/dynamic-modal.vue
@@ -6,31 +6,41 @@
       <img src="~@/assets/imgs/icons/arrow.png">
     </div>
 
-    <div v-if="clicked === 'checkin'" class="checkin-modal"></div>
+    <div v-if="clicked === 'checkin'" class="checkin-modal">
+         <div class="block">
+    <span class="demonstration">Default</span>
+    <el-date-picker
+      v-model="value1"
+      type="daterange"
+      range-separator="To"
+      start-placeholder="Start date"
+      end-placeholder="End date">
+    </el-date-picker>
+  </div>
+    </div>
     <div v-if="clicked === 'checkout'" class="checkout-modal"></div>
 
     <div v-if="clicked === 'guests'" class="guests-modal">
       <form>
         <label class="expanded-search-label" for="adults">
-          <span>Adults</span>
-          <span class="sub-header-lable">Ages 13 or above</span>
-          <input class="change-amount" type="number" />
-
+            <span>Adults</span>
+          <span>Ages 13 or above</span>
+          <el-input-number v-model="num" @change="handleChange" :min="0" :max="10"></el-input-number>
         </label>
         <label class="expanded-search-label" for="children">
-          <span>Children</span>
-          <span class="sub-header-lable" >Ages 2-12</span>
-          <input type="number" />
+                <span>Children</span>
+                <span>Ages 2-12</span>
+          <el-input-number v-model="num" @change="handleChange" :min="0" :max="10"></el-input-number>
         </label>
         <label class="expanded-search-label" for="infants">
-          <span>Infats</span>
-          <span class="sub-header-lable">Under 2</span>
-          <input type="number" />
+          <span>Infants</span>
+          <span>Under 2</span>
+          <el-input-number v-model="num" @change="handleChange" :min="0" :max="10"></el-input-number>
         </label>
         <label class="expanded-search-label" for="pets">
           <span>Pets</span>
           <span class="sub-header-lable">Bringing an assitance animal?</span>
-          <input type="number" />
+          <el-input-number v-model="num" @change="handleChange" :min="0" :max="10"></el-input-number>
         </label>
       </form>
 
@@ -44,6 +54,16 @@
 export default {
   name: "Dynamic Modal",
   props: ["clicked"],
+   data() {
+      return {
+        num: 0,
+      };
+    },
+    methods: {
+      handleChange(value) {
+        console.log(value)
+      }
+    }
 };
 </script>
 
