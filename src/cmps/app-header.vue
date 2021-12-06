7e7d3634733d0f1bd0e45057ea549aff76ce8506
Einav Cohen <einavsprojects@gmail.com>
2021-12-06 22:49:31 +0200

    moved login to cmps

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -65,98 +65,7 @@
       <div class="global">
         <img :src="require(`@/assets/imgs/icons/${globalSrc}.png`)" />
       </div>
-      <div @click="openCloseMenu" class="user-options">
-        <div class="burger">☰</div>
-        <img class="avatar" src="../assets/imgs/icons/avatar.png" />
-        <div v-if="isMenuOpen" class="profile-menu">
-          <button @click="openLogin">Login</button>
-          <button @click="openSignin">Signup</button>
-        </div>
-      </div>
-      <section v-if="isLoginOpen" class="login-modal">
-        <div class="login-header">
-          <button class="x-close" @click="closeLogin">
-            <span class="material-icons"> close </span>
-          </button>
-          <p>Log in</p>
-        </div>
-        <div class="login-input">
-          <div class="input-txt"><h2>Welcome to stayinn</h2></div>
-          <form action="">
-            <input type="text" placeholder="Enter email or username" /><input
-              type="password"
-              placeholder="Enter password"
-            /><button class="checkout-btn" style="--x: 394px; --y: 47px">
-              <span>Continue</span>
-            </button>
-          </form>
-          <div class="move-signup-button">
-            <small
-              >Dont have a user? click here to
-              <button class="x-close" @click="openSigninCloseLogin">
-                sign up
-              </button></small
-            >
-          </div>
-        </div>
-        <div class="login-divider">
-          <div class="border"></div>
-          <small>or</small>
-        </div>
-        <div class="login-socials">
-          <button>
-            <span class="material-icons" style="color: rgb(66, 103, 178)">
-              facebook </span
-            >Continue with Facebook</button
-          ><button>
-            <img src="@/assets/imgs/icons/google.png" alt="" /> Continue with
-            Google
-          </button>
-        </div>
-      </section>
-      <section v-if="isSigninOpen" class="signin-modal">
-        <div class="login-header">
-          <button class="x-close" @click="closeSignin">
-            <span class="material-icons"> close </span>
-          </button>
-          <p>Sign up</p>
-        </div>
-        <div class="login-input">
-          <div class="input-txt"><h2>Welcome to stayinn</h2></div>
-          <form action="">
-            <input type="text" placeholder="Enter email or username" />
-            <input type="text" placeholder="Enter full name" />
-            <input type="password" placeholder="Enter password" /><button
-              class="checkout-btn"
-              style="--x: 394px; --y: 47px"
-            >
-              <span>Continue</span>
-            </button>
-          </form>
-          <div class="move-signup-button">
-            <small
-              >Dont have a user? click here to
-              <button class="x-close" @click="openLoginCloseSignin">
-                login
-              </button></small
-            >
-          </div>
-        </div>
-        <div class="login-divider">
-          <div class="border"></div>
-          <small>or</small>
-        </div>
-        <div class="login-socials">
-          <button>
-            <span class="material-icons" style="color: rgb(66, 103, 178)">
-              facebook </span
-            >Continue with Facebook</button
-          ><button>
-            <img src="@/assets/imgs/icons/google.png" alt="" /> Continue with
-            Google
-          </button>
-        </div>
-      </section>
+      <login />
     </section>
   </header>
 </template>
@@ -164,6 +73,7 @@
 import dynamicModal from "./dynamic-modal.vue";
 import DatePicker from "vue2-datepicker";
 import "vue2-datepicker/index.css";
+import login from './login.vue'
 DatePicker.methods.displayPopup = function () {
   this.position = {
     left: 0,
@@ -174,6 +84,7 @@ export default {
   components: {
     DatePicker,
     dynamicModal,
+    login
   },
   data() {
     return {
@@ -186,9 +97,6 @@ export default {
       expandedSearch: false,
       clickedOn: "",
       time3: null,
-      isMenuOpen: false,
-      isLoginOpen: false,
-      isSigninOpen: false,
     };
   },
   created() {
@@ -255,34 +163,6 @@ export default {
     openModal(of) {
       this.clickedOn = of;
       // console.log(this.clickedOn)
-    },
-    openCloseMenu() {
-      console.log();
-      if (this.isMenuOpen === false) {
-        this.isMenuOpen = true;
-      } else {
-        this.isMenuOpen = false;
-      }
-    },
-    openLogin() {
-      this.isLoginOpen = true;
-    },
-    closeLogin() {
-      this.isLoginOpen = false;
-    },
-    openSignin() {
-      this.isSigninOpen = true;
-    },
-    closeSignin() {
-      this.isSigninOpen = false;
-    },
-    openSigninCloseLogin() {
-      this.isSigninOpen = true;
-      this.isLoginOpen = false;
-    },
-    openLoginCloseSignin() {
-      this.isLoginOpen = true;
-      this.isSigninOpen = false;
     },
   },
   computed: {
---
+++ src/cmps/login.vue
@@ -0,0 +1,141 @@
+<template>
+  <section class="login-container">
+    <div @click="openCloseMenu" class="user-options">
+      <div class="burger">☰</div>
+      <img class="avatar" src="../assets/imgs/icons/avatar.png" />
+      <div v-if="isMenuOpen" class="profile-menu">
+        <button @click="openLogin">Login</button>
+        <button @click="openSignin">Signup</button>
+      </div>
+    </div>
+    <section v-if="isLoginOpen" class="login-modal">
+      <div class="login-header">
+        <button class="x-close" @click="closeLogin">
+          <span class="material-icons"> close </span>
+        </button>
+        <p>Log in</p>
+      </div>
+      <div class="login-input">
+        <div class="input-txt"><h2>Welcome to stayinn</h2></div>
+        <form action="">
+          <input type="text" placeholder="Enter email or username" /><input
+            type="password"
+            placeholder="Enter password"
+          /><button class="checkout-btn" style="--x: 394px; --y: 47px">
+            <span>Continue</span>
+          </button>
+        </form>
+        <div class="move-signup-button">
+          <small
+            >Dont have a user? click here to
+            <button class="x-close" @click="openSigninCloseLogin">
+              sign up
+            </button></small
+          >
+        </div>
+      </div>
+      <div class="login-divider">
+        <div class="border"></div>
+        <small>or</small>
+      </div>
+      <div class="login-socials">
+        <button>
+          <span class="material-icons" style="color: rgb(66, 103, 178)">
+            facebook </span
+          >Continue with Facebook</button
+        ><button>
+          <img src="@/assets/imgs/icons/google.png" alt="" /> Continue with
+          Google
+        </button>
+      </div>
+    </section>
+    <section v-if="isSigninOpen" class="signin-modal">
+      <div class="login-header">
+        <button class="x-close" @click="closeSignin">
+          <span class="material-icons"> close </span>
+        </button>
+        <p>Sign up</p>
+      </div>
+      <div class="login-input">
+        <div class="input-txt"><h2>Welcome to stayinn</h2></div>
+        <form action="">
+          <input type="text" placeholder="Enter email or username" />
+          <input type="text" placeholder="Enter full name" />
+          <input type="password" placeholder="Enter password" /><button
+            class="checkout-btn"
+            style="--x: 394px; --y: 47px"
+          >
+            <span>Continue</span>
+          </button>
+        </form>
+        <div class="move-signup-button">
+          <small
+            >Dont have a user? click here to
+            <button class="x-close" @click="openLoginCloseSignin">
+              login
+            </button></small
+          >
+        </div>
+      </div>
+      <div class="login-divider">
+        <div class="border"></div>
+        <small>or</small>
+      </div>
+      <div class="login-socials">
+        <button>
+          <span class="material-icons" style="color: rgb(66, 103, 178)">
+            facebook </span
+          >Continue with Facebook</button
+        ><button>
+          <img src="@/assets/imgs/icons/google.png" alt="" /> Continue with
+          Google
+        </button>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+      isLoginOpen: false,
+      isSigninOpen: false,
+    };
+  },
+  methods: {
+    openCloseMenu() {
+      console.log();
+      if (this.isMenuOpen === false) {
+        this.isMenuOpen = true;
+      } else {
+        this.isMenuOpen = false;
+      }
+    },
+    openLogin() {
+      this.isLoginOpen = true;
+    },
+    closeLogin() {
+      this.isLoginOpen = false;
+    },
+    openSignin() {
+      this.isSigninOpen = true;
+    },
+    closeSignin() {
+      this.isSigninOpen = false;
+    },
+    openSigninCloseLogin() {
+      this.isSigninOpen = true;
+      this.isLoginOpen = false;
+    },
+    openLoginCloseSignin() {
+      this.isLoginOpen = true;
+      this.isSigninOpen = false;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
