c2b228de84baa3c982d17a060541ae5c52eff1c0
tal ben-avi <tal@ben-avi.com>
2021-12-07 10:54:00 +0200

    ss

--- src/cmps/login.vue
+++ src/cmps/login.vue
@@ -168,7 +168,7 @@ export default {
     },
     login() {
       userService.login(this.user).then(() => {
-        // myBus.$emit("logged");
+        // eventBus.$emit("logged");
         this.$router.push("/");
       });
     },
